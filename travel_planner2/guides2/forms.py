8ab9b371dc12b8c72babad94a600d2ae96201850
Adrian20064 <carrascoadrian2006@gmail.com>
2025-07-31 17:35:51 -0700

    new chnage

--- travel_planner2/guides2/forms.py
+++ travel_planner2/guides2/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 class TravelForm(forms.Form):
     start_city = forms.ChoiceField(choices=[])  
-    kilometers = forms.ChoiceField(choices=[])
+    kilometers = forms.IntegerField(min_value=1, label="Maximum Driving Distance (km)")
 
     def __init__(self, *args, **kwargs):
-        cities = kwargs.pop('cities', [])  
+        cities = kwargs.pop('cities', [])
         super().__init__(*args, **kwargs)
         city_choices = [(city, city) for city in cities]
         self.fields['start_city'].choices = city_choices
